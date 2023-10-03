db4f5b0765f6554bcaae3b7ce15ddadf67e64565
Anna Aksenov <annaaksenov8@gmail.com>
2023-10-03 22:24:22 +0000

    Completed issue 5, create navigation bar without functionality

--- client/src/Journal.jsx
+++ client/src/Journal.jsx
@@ -1,1 +1,25 @@
-export default function Journal() {}
+import NavBar from './NavBar';
+// import EntryView from "./EntryView";
+
+export default function Journal() {
+  return (
+    <>
+      <header className="header purple-background">
+        <div className="container">
+          <div className="row">
+            <div className="column-full d-flex align-center">
+              <NavBar />
+            </div>
+          </div>
+        </div>
+      </header>
+      <main>
+        <div className="container">
+          {/*<EntryView />
+            <Entries />
+            */}
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ client/src/NavBar.jsx
@@ -0,0 +1,12 @@
+export default function NavBar() {
+  return (
+    <>
+      <h1 className="white-text">Code Journal</h1>
+      <h3>
+        <a id="entriesLink" className="entries-link white-text">
+          Entries
+        </a>
+      </h3>
+    </>
+  );
+}
